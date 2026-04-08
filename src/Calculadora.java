ff8e96bf7812d8f4e12cb9bf83e13c5589b7f6c3
Andre Nogueira Barreto <andresinxp@hotmail.com>
2026-04-08 14:30:51 -0300

    Initial commit

---
+++ src/Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+
+    int recebeDobra (int numero){
+        int numeroDobrado = numero * 2;
+        return numeroDobrado;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.recebeDobra(10);
+
+        System.out.println(resultado);
+
+    }
+}
